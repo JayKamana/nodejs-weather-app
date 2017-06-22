3e6b2a38730d8455100661a30fecb96a4a196dd8
James Kamana <jaykamana@gmail.com>
2017-06-22 22:43:46 +0300

    initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+  MY_KEY : 'ead4a0e346b6349fd459bf22f8ed5289'
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+let request = require('request');
+
+let apiKey = config.MY_KEY;
+let city = 'lusaka';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
